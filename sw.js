8c0369c21c9125841f7c25d453ecbe7e5fe4152c
Kleitomberg <51670381+Kleitomberg@users.noreply.github.com>
2019-12-14 14:25:31 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,45 @@
+const staticCacheName = 'OBS_2019_12_14_13_45'
+
+this.addEventListener('install', event => {
+    this.skipWaiting();
+
+    event.waitUntil(
+        caches.open(staticCacheName)
+        .then(cache => {
+            return cache.addAll([
+                '/',
+                'https://kleitomberg.github.io/',
+                'index.js',
+                'imagens/sheet.png',
+                'manifest.json',
+                'imagens/icons/icon-192x192.png',
+                'chrome-extension://elicpjhcidhpjomhibiffojpinpmmpil/video/chromecastcheck.js',
+               ' https://kleitomberg.github.io/imagens/icons/icon-144x144.png',           
+            ])
+        })
+    )
+})
+
+this.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.filter(cacheName => cacheName.startsWith('usd'))
+                .filter(cacheName => cacheName !== staticCacheName)
+                .map(cacheName => caches.delete(cacheName))
+            )
+        })
+    )
+})
+
+this.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+        .then(response => {
+            return response || fetch(event.request)
+        })
+        .catch(() => {
+            return caches.match('/')
+        })
+    )
+})
